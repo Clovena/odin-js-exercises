e660ab8d7bfe0f38031e72a9713e8d22f261bf6a
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-02-24 16:38:03 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  if (str === '') {
+    return '';
+  } else {
+    let rev = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+      rev += str[i];
+    }
+    return rev;
+  }
 };
 
 // Do not edit below this line
