b0f9b9a5900c4e22626db0e4d8d876d8c72f2fed
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-02-24 16:30:56 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    let rep = "";
+    for (let i = 0; i < num; i++) {
+      rep += `${str}`;
+    }
+    return rep;
+  }
 };
 
 // Do not edit below this line
