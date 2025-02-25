6c1e8ad7743801bc5acec1d69706576543a66403
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-02-24 19:10:53 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  if (
+    // check that typeof is "number" for both start and end
+    (typeof start != "number") |
+    (typeof end != "number") |
+    // check that both start and end are integers
+    (start % 1 != 0) |
+    (end % 1 != 0) | 
+    // check that both start and end are positive 
+    (start < 0) | 
+    (end < 0)
+  ) {
+    // return 'ERROR' if not
+    return "ERROR";
+  } else {
+    runningSum = 0;
+    // define lower and upper numbers
+    const upper = Math.max(start, end);
+    const lower = Math.min(start, end);
+    // then, iterate through range bounded by [upper, lower]
+    for (i = lower; i <= upper; i++) {
+      // calculate running total using +=
+      runningSum += i;
+    }
+    return runningSum;
+  }
 };
 
 // Do not edit below this line
