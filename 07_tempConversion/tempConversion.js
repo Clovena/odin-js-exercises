17067d6a5b72265fb8a1e0fed7f3780a190b63e9
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-02-24 19:31:42 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const roundToTenths = function (number) {
+  return Math.round(number * 10) / 10;
+}
+
+const convertToCelsius = function (temp) {
+  let celsius = (temp - 32) * (5 / 9);
+  return roundToTenths(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = (temp * 9 / 5) + 32;
+  return roundToTenths(fahrenheit);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
